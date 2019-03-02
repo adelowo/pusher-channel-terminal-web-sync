98c64362bc9499c8b29d711c7ae4edc64f14a391
Lanre Adelowo <yo@lanre.wtf>
2019-03-02 16:21:28 +0100

    allow compatibility with windows by using a go script as against straight up bash

--- uuid.sh
+++
@@ -1,6 +0,0 @@
-while true
-do
-    uuid=`od -x /dev/urandom | head -1 | awk '{OFS="-"; print $2$3,$4,$5,$6,$7$8$9}'`
-    echo "New UUID generated --- $uuid"
-    sleep 1
-done
---
+++ uuid/uuid.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+
+	for {
+		fmt.Printf("Generating a new UUID -- %s", uuid.New())
+		fmt.Println()
+		time.Sleep(time.Second)
+	}
+}
